6ad0390c3d79b040899bd1d17eeffd6ca1264084
João Pedro Porto Teixeira <joao.porto.ismart@gmail.com>
2023-03-29 18:57:54 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import router from "./routes";
+import { RouterProvider } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import {
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+} from "react-router-dom";
+import Home from "../pages/Home";
+
+const RoutesJSX = <Route path="/" element={<Home />}></Route>;
+
+const routes = createRoutesFromElements(RoutesJSX);
+
+const router = createBrowserRouter(routes);
+
+export default router;
