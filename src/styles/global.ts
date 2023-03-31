c1ba424ea8afa433da9778d2ece2f12f651b0282
João Pedro Porto Teixeira <joao.porto.ismart@gmail.com>
2023-03-31 11:35:43 -0300

    feat: Global Style and Logos assets

---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    list-style: none;
+    text-decoration:none;
+  }
+
+  :root {
+    --red0: #8C0810;
+    --red100: #54080F;
+    --red200: #41080F;
+    --red300: #120000;
+
+    --black0: #212529;
+    --black100: #0A0A0A;
+    --black200: #000811;
+    --black300: #000000;
+
+    --white: #FFFFFF;
+
+    --inter: 'Inter', sans-serif;
+  }
+
+  body {
+    min-width: 100%;
+    min-height: 100vh;
+    background-color: var(--black200);
+  }
+
+  html{
+    scroll-behavior: smooth;
+  }
+
+  button, div, label, select, h1, h2, h3, h4, h5, h6, input, nav, ul, li {
+    font-family: var(--inter);
+    color: var(--white);
+  }
+`;
---
+++ src/utils/stackData.ts
@@ -0,0 +1,23 @@
+import imgCSS from "../images/logos/css.svg";
+import imgDocker from "../images/logos/docker.svg";
+import imgHTML from "../images/logos/html.svg";
+import imgJS from "../images/logos/js.svg";
+import imgNodeJS from "../images/logos/nodejs.svg";
+import imgPostgreSQL from "../images/logos/postgresql.svg";
+import imgPython from "../images/logos/python.svg";
+import imgReact from "../images/logos/react.svg";
+import imgStyled from "../images/logos/styled.svg";
+import imgTypeScript from "../images/logos/typecript.svg";
+
+export const stackData = [
+  { title: "HTML", img: imgHTML },
+  { title: "CSS", img: imgCSS },
+  { title: "JavaScript", img: imgJS },
+  { title: "React", img: imgReact },
+  { title: "Styled Components", img: imgStyled },
+  { title: "TypeScript", img: imgTypeScript },
+  { title: "Node.js", img: imgNodeJS },
+  { title: "Python", img: imgPython },
+  { title: "Docker", img: imgDocker },
+  { title: "Postgre SQL", img: imgPostgreSQL },
+];
